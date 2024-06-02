cd9372a43f5c6a48c7aa2d570a382970338bc615
Abubakr <thefox5000@gmail.com>
2024-06-02 13:06:32 +0300

    Create DataTable with coloumns and rows

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO___DataTables___DataSets__DataAdapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable Employees = new DataTable();
+
+            Employees.Columns.Add("ID", typeof(int));
+            Employees.Columns.Add("Name", typeof(string));
+            Employees.Columns.Add("Salary", typeof(double));
+            Employees.Columns.Add("Country", typeof(string));
+
+            Employees.Rows.Add("1", "Abubakr Ali", 5000, "Sudan");
+            Employees.Rows.Add("2", "Ahmad", 1000, "Jordan");
+
+            int numberOfEmployees = Employees.Rows.Count;
+            double totalSalaries = Convert.ToDouble(Employees.Compute("SUM(Salary)", string.Empty));
+            double avgSalaries = Convert.ToDouble(Employees.Compute("AVG(Salary)", string.Empty));
+
+            Console.WriteLine($" Employees Count: {numberOfEmployees}");
+            Console.WriteLine($"Total Salaryies: {totalSalaries}");
+            Console.WriteLine($"Average Salaryies: {avgSalaries}");
+        }
+    }
+}
